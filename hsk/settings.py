7648032b10e51c3164be7297168934fcb32850dc
v1r4m <choyoung7789@gmail.com>
2024-01-04 16:52:40 +0900

    getting ready for an api

--- hsk/settings.py
+++ hsk/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prompt'
+    'prompt',
+    'rest-framework'
 ]
 
 MIDDLEWARE = [
