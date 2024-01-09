2e87862b16c52288504bcc78b659e204270a4e2e
v1r4m <choyoung7789@gmail.com>
2024-01-09 13:30:04 +0900

    deploy

--- hsk/settings.py
+++ hsk/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hskai.pythonanywhere.com'
+]
 
 
 # Application definition
