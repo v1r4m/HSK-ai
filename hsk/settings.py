a17832a248537d0924c032d1a3918fb43d0a7e0e
v1r4m <choyoung7789@gmail.com>
2024-01-12 16:52:57 +0900

    tailwindcss, node20, 선택지 적용

--- hsk/settings.py
+++ hsk/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'localhost',
     'hskai.pythonanywhere.com'
 ]
 
@@ -124,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'prompt'/'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
